6feb71f42f8ef895e838c7959b4799389fd9cc14
Lanreath <tyfei00@gmail.com>
2023-10-10 17:47:38 +0300

    Complete 05,06,07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let r = (f - 32) * (5/9);
+  r = Math.round(r * 10) / 10;
+  return r
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let r = c * (9/5) + 32;
+  r = Math.round(r * 10) / 10;
+  return r
 };
 
 // Do not edit below this line
